95be8d844633bea7eece9a770b5c7a194c5e87d7
Alisamar Husain <zrthxn@gmail.com>
2019-02-26 16:31:03 +0530

    CORS

--- server.js
+++ server.js
@@ -69,8 +69,8 @@ frontend.get('/', (req,res)=>{
 // APIs ------------------------------ APIs //
 // ======================================= //
 api.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
 
