1e3b0af3bc86c5233f8b5c86a4fa8572acb9c795
Alisamar Husain <zrthxn@gmail.com>
2019-03-03 08:38:59 +0530

    Test ALPR

--- util/EngineManager.js
+++ util/EngineManager.js
@@ -1,39 +1,42 @@
 const os = require('os')
 const fs = require('fs')
 const cluster = require('cluster')
+const request = require('request')
+const alpr = require("node-openalpr")
 const {spawn, fork, exec} = require('child_process')
 
 exports.runEngine = (input, contentType) => {
-    let genFileName = 'med_', extn
+    let genFileName = 'med_', extn, filepath
 
-    // for(let i=0;i<20;i++)
-    //     genFileName += Math.floor( Math.random()*16 ).toString(16)
+    for(let i=0;i<20;i++)
+        genFileName += Math.floor( Math.random()*16 ).toString(16)
     
-    // switch(contentType) {
-    //     case 'image/jpeg':
-    //         extn = '.jpeg'
-    //         break
-    //     case 'image/png':
-    //         extn = '.png'
-    //         break
-    //     case '':
-    //         extn = '.mp4'
-    //         break
-    //     default: 
-    //         extn = ''
-    // }
+    switch(contentType) {
+        case 'image/jpeg':
+            extn = '.jpeg'
+            break
+        case 'image/png':
+            extn = '.png'
+            break
+        case '':
+            extn = '.mp4'
+            break
+        default: 
+            extn = ''
+    }
 
-    // fs.writeFileSync('./inputmedia/' + genFileName + extn, input)
+    filepath = __dirname + '/inputmedia/' + genFileName + extn
+    fs.writeFileSync(filepath, input)
     console.time('Engine Run')
-    // const engine = spawn('python', [
-    //     './Engine/engine.py', 
-    //     'FUCKALL', 
-    //     // contentType
-    // ])
+    const engine = spawn('python', [
+        './Engine/engine.py', 
+        'FUCKALL', 
+        // contentType
+    ])
 
     const engine = exec('python', [
         './Engine/engine.py', 
-        'FUCKALL', 
+        , 
         // contentType
     ])
     
@@ -56,4 +59,24 @@ exports.runEngine = (input, contentType) => {
     //         console.log('ENGINE FAILED')
     //     engine.unref()
     // })
+}
+
+exports.run = () => {
+
+}
+
+exports.TestAPI = () => {
+    const path = './inputmedia/Sequence 01070.jpg'
+
+    alpr.Start()
+    alpr.GetVersion()
+
+    console.log(alpr.IdentifyLicense(path, function (error, output) {
+        var results = output.results
+        console.log (id +" "+ output.processing_time_ms +" "+ ((results.length > 0) ? results[0].plate : "No results"))
+    
+        if (id == 349) {
+            console.log (alpr.Stop ())
+        }
+    }))
 }
